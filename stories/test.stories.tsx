1673e433c4984f9824d8d9bd80c7df4251b4ab7b
Dmitry Spiridonov <szpiridonov@gmail.com>
2019-05-10 00:42:10 +0300

    remove unnecessarry files

--- stories/test.stories.tsx
+++
@@ -1,7 +0,0 @@
-import * as React from 'react'
-import { storiesOf } from '@storybook/react'
-import App from '../src/App'
-
-storiesOf('AppStory', module).add('Authorise', () => {
-    return <App />
-})
