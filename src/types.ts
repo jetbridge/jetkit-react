2fb48895f624ca54cf2f832f6205bb37ac031440
Dmitry Spiridonov <szpiridonov@gmail.com>
2019-05-24 11:05:35 +0300

    fix icon props

--- src/types.ts
+++ src/types.ts
@@ -1,7 +1,9 @@
+import SvgIcon, { SvgIconProps } from '@material-ui/core/SvgIcon'
+
 export interface IMenuSection {
     title: string
     path: string
     subsections?: IMenuSection[]
     expanded?: boolean
-    icon?: React.ElementType | HTMLElement
+    icon?: React.ElementType | HTMLElement | React.ComponentType<SvgIconProps>
 }
