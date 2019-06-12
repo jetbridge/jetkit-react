bef64ce559915e32c23f88b1d27429e215a2b95b
Mischa Spiegelmock <mischa@mvstg.biz>
2019-06-12 15:26:31 +0300

    WIP

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,13 @@
 import { apiClient } from './apiClient/index'
 import LoginScreen from './LoginScreen'
-import {
-    authReducer,
-    IAuthenticatedApplicationState,
-    isAuthenticated,
-    storeAccessToken,
-    getAccessToken,
-    getFreshToken,
-    authenticate,
-    IUser,
-    IUserCredential,
-} from './store/auth'
+import { authReducer, IAuthenticatedApplicationState, authenticate, IUser, IUserCredential } from './store/auth'
+import authService from './service/auth'
 
 // exporting AuthRedux as a separate entity
 // TODO: add more documentation and interfaces
 const AuthRedux = {
-    authReducer,
-    isAuthenticated,
-    storeAccessToken,
-    getAccessToken,
-    getFreshToken,
-    authenticate,
+  authReducer,
+  authenticate,
 }
 
-export { LoginScreen, apiClient, AuthRedux, IAuthenticatedApplicationState, IUser, IUserCredential }
+export { LoginScreen, apiClient, AuthRedux, IAuthenticatedApplicationState, IUser, IUserCredential, authService }
--- src/store/auth.ts
+++ src/store/auth.ts
@@ -1,105 +1,58 @@
 import { Dispatch, Reducer } from 'redux'
 import { createStandardAction, getType } from 'typesafe-actions'
-import { login, refreshToken, IRefreshTokenResponse } from '../apiClient/auth'
+import authService from '../service/auth'
 
 export interface IAction<T> {
-    type: string
-    payload: T
+  type: string
+  payload: T
 }
 
 export interface IAuthenticatedApplicationState {
-    auth: IUserCredential
+  auth: IUserCredential
 }
 
 export type GetState = () => IAuthenticatedApplicationState
-export interface ICredentials {
-    readonly refresh_token: string
-    readonly access_token: string
-}
 
-export type IUserType = 'client' | 'stuff'
+export type IUserType = 'client' | 'staff'
 
 export interface IUser {
-    readonly id?: number
-    readonly name?: string
-    readonly email?: string
-    readonly user_type?: IUserType
+  readonly id?: number
+  readonly name?: string
+  readonly email?: string
+  readonly user_type?: IUserType
 }
 
 export interface IUserCredential {
-    readonly tokens?: ICredentials
-    readonly user?: IUser
+  readonly user?: IUser
 }
 
 export const initialState: IUserCredential = {}
 
-export const storeAccessToken = createStandardAction('auth/storeAccessToken')<string>()
-export const storeCredentials = createStandardAction('auth/storeCreds')<IUserCredential>()
+export const storeAuthResponse = createStandardAction('auth/storeCreds')<IUserCredential>()
 
 const reducer: Reducer<IUserCredential> = (state = initialState, action) => {
-    switch (action.type) {
-        case getType(storeAccessToken): {
-            if (!state.tokens || !state.tokens.refresh_token) {
-                console.error('got storeAccessToken but we are missing refresh token. cannot continue.')
-                return state
-            }
-            return {
-                ...state,
-                tokens: {
-                    ...state.tokens,
-                    access_token: action.payload,
-                },
-            }
-        }
-        case getType(storeCredentials): {
-            return {
-                ...state,
-                tokens: {
-                    access_token: action.payload.access_token,
-                    refresh_token: action.payload.refreshToken,
-                },
-                user: {
-                    name: action.payload.user.name,
-                },
-            }
-        }
-        default: {
-            return state
-        }
+  switch (action.type) {
+    case getType(storeAuthResponse): {
+      return {
+        ...state,
+        user: {
+          name: action.payload.user.name,
+        },
+      }
     }
+    default: {
+      return state
+    }
+  }
 }
 
 export type authState = ReturnType<typeof reducer>
 
 export function authenticate(email: string, password: string) {
-    return async (dispatch: Dispatch, getState: GetState) => {
-        const credentials = await login(email, password)
-        return dispatch(storeCredentials(credentials))
-    }
-}
-
-export function getFreshToken() {
-    return async (dispatch: Dispatch, getState: GetState): Promise<ICredentials | IRefreshTokenResponse> => {
-        const creds = credentialsSelector(getState())
-        const token = creds ? creds.refresh_token : null
-        if (!token) return Promise.reject('No refresh token')
-
-        // refresh
-        return await refreshToken(token)
-    }
-}
-
-export const credentialsSelector = (state: IAuthenticatedApplicationState): ICredentials | undefined => {
-    return state.auth.tokens
-}
-
-export const getAccessToken = () => (dispatch: Dispatch, getState: GetState) => {
-    const creds = credentialsSelector(getState())
-    return creds ? creds.access_token : null
-}
-
-export const isAuthenticated = (state: IAuthenticatedApplicationState): boolean => {
-    return !!(state.auth.tokens && state.auth.tokens.access_token)
+  return async (dispatch: Dispatch, getState: GetState) => {
+    const res = await authService.login(email, password)
+    return dispatch(storeAuthResponse(res))
+  }
 }
 
 export { reducer as authReducer }
