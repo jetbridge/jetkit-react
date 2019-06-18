84d6114d9d210243baff1c453226ebbd1df5ed6f
Dmitry Spiridonov <szpiridonov@gmail.com>
2019-06-18 17:38:29 +0300

    provide default notification messages

--- src/snackbarCustom/notify.ts
+++ src/snackbarCustom/notify.ts
@@ -1,11 +1,11 @@
 import { SNACKBAR_NOTIF_EVENT } from './useSnackbar'
 
 const notify = {
-  success: (message: string = '') => {
+  success: (message: string = 'Success') => {
     const notifyEvent = new CustomEvent(SNACKBAR_NOTIF_EVENT, { detail: { message, messageType: 'success' } })
     document.dispatchEvent(notifyEvent)
   },
-  warning: (message: string = '') => {
+  warning: (message: string = 'Error') => {
     const notifyEvent = new CustomEvent(SNACKBAR_NOTIF_EVENT, { detail: { message, messageType: 'warning' } })
     document.dispatchEvent(notifyEvent)
   },
