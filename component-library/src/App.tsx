160786977cb431ab399d8995bfa976dfea531d33
Mischa Spiegelmock <mischa@mvstg.biz>
2019-07-05 15:17:46 +0300

    Spelling, versions

--- component-library/src/App.tsx
+++ component-library/src/App.tsx
@@ -3,7 +3,7 @@ import * as React from 'react'
 const App: React.FC = () => {
   return (
     <div className="App">
-      <h3>New JetBrige App!</h3>
+      <h3>New JetBridge App!</h3>
       <p>Enjoy!</p>
     </div>
   )
