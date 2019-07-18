af7db45c52ea2f2921b9a9f70c088b50d76e6b99
Mischa Spiegelmock <mischa@mvstg.biz>
2019-07-18 12:32:19 +0300

    Make login take user type

--- src/service/auth.ts
+++ src/service/auth.ts
@@ -3,7 +3,7 @@ import { IUserCredential } from '../store/auth'
 import { setAuthTokens } from 'axios-jwt'
 
 export default {
-  login: async (email: string, password: string): Promise<IUserCredential> => {
+  login: async <UT>(email: string, password: string): Promise<IUserCredential & UT> => {
     const res = (await apiClient.post('/auth/login', { email, password })).data
     setAuthTokens(authResponseToAuthTokens(res))
     return res
