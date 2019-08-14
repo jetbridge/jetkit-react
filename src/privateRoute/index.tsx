a98adeaffd0d54080463bcefbd945e1c75536f47
Adam Szeptycki <adamszeptycki@gmail.com>
2019-08-14 17:45:28 +0200

    fallbackRoute

--- src/privateRoute/index.tsx
+++ src/privateRoute/index.tsx
@@ -3,14 +3,14 @@ import { Route, Redirect, RouteProps } from 'react-router'
 import { isLoggedIn } from 'axios-jwt'
 
 interface IPrivateRotueProps extends RouteProps {
-  fallBackRoute: string
+  fallbackRoute: string
 }
 
 function PrivateRoute(props: IPrivateRotueProps) {
   if (isLoggedIn) {
     return <Route {...props} />
   }
-  return <Redirect to={props.fallBackRoute} />
+  return <Redirect to={props.fallbackRoute} />
 }
 
 export default PrivateRoute
