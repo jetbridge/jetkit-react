bb508d2843b345c2caaab3f29adac8d11a5eec72
Dmitry Spiridonov <szpiridonov@gmail.com>
2019-08-21 17:24:54 +0300

    set up storybook with typescript

---
+++ component-library/src/stories/inputFields.stories.tsx
@@ -0,0 +1,17 @@
+import * as React from 'react'
+import { storiesOf } from '@storybook/react'
+import TextFieldWithDebounce from '../../../src/TextFieldWithDebounce'
+
+storiesOf('Input Fields', module).add('TextFieldWithDebounce', () =>
+  React.createElement(() => {
+    const [value] = React.useState('test')
+    const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+      console.debug(event.target.value)
+    }
+    return (
+      <div style={{ width: 500 }}>
+        <TextFieldWithDebounce onChange={handleChange} value={value} />
+      </div>
+    )
+  })
+)
