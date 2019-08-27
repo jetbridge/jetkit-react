68262d80e6d0aeb6896a9331a3a3fb070e40c9b7
Mischa Spiegelmock <mischa@mvstg.biz>
2019-08-27 13:45:41 +0300

    bleh

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,9 @@
 module.exports = {
-  parser: '@typescript-eslint/parser',
-  extends: ['plugin:@typescript-eslint/recommended', 'plugin:react/recommended'],
+  parser: '@typescript-eslint/parser', // Specifies the ESLint parser
+  extends: [
+    'plugin:react/recommended', // Uses the recommended rules from @eslint-plugin-react
+    'plugin:@typescript-eslint/recommended', // Uses the recommended rules from @typescript-eslint/eslint-plugin
+  ],
   parserOptions: {
     ecmaFeatures: {
       jsx: true,
