6250517467e3815d27c73b25cc40c63e3a285bd0
Mischa Spiegelmock <mischa@mvstg.biz>
2019-08-27 18:53:34 +0300

    pass through dropzone options

--- src/form/fileDropzone/index.tsx
+++ src/form/fileDropzone/index.tsx
@@ -1,4 +1,4 @@
-import { useDropzone } from 'react-dropzone'
+import { useDropzone, DropzoneOptions } from 'react-dropzone'
 import * as React from 'react'
 import { makeStyles } from '@material-ui/styles'
 import classNames from 'classnames'
@@ -126,7 +126,7 @@ const useStyles = makeStyles({
   },
 })
 
-export interface IFileDropzoneProps {
+export interface IFileDropzoneProps extends DropzoneOptions {
   value?: IFileDropzone
   isImage?: boolean
   title?: string
@@ -153,8 +153,11 @@ const FileDropzone = (props: IFileDropzoneProps) => {
     editable = true,
     fullSize,
     disabled,
+    ...dropzoneOptions
   } = props
-  const { getRootProps, getInputProps, isDragActive } = useDropzone({ onDrop })
+
+  // react-dropzone
+  const { getRootProps, getInputProps, isDragActive } = useDropzone({ onDrop, ...dropzoneOptions })
   const classes = useStyles(props)
 
   // render existing image preview if we have one and user hasn't selected a new file
