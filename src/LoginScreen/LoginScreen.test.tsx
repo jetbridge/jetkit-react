cc6c0e97bf0d5aa813accab2c10c767cb426d4fa
Mischa Spiegelmock <mischa@mvstg.biz>
2019-09-05 18:07:58 +0300

    lint

--- src/LoginScreen/LoginScreen.test.tsx
+++ src/LoginScreen/LoginScreen.test.tsx
@@ -3,7 +3,7 @@ import * as ReactDOM from 'react-dom'
 import LoginScreen from '.'
 
 it('renders without crashing', () => {
-    const div = document.createElement('div')
-    ReactDOM.render(<LoginScreen onSubmitClick={() => null} />, div)
-    ReactDOM.unmountComponentAtNode(div)
+  const div = document.createElement('div')
+  ReactDOM.render(<LoginScreen onSubmitClick={() => null} />, div)
+  ReactDOM.unmountComponentAtNode(div)
 })
