9755db490aaa5205171964cac85eae4461b7329b
Dizzzmas <yankovskyi.dmytro736@gmail.com>
2020-03-09 10:07:03 +0200

    Added signUp to authService

--- src/service/auth.ts
+++ src/service/auth.ts
@@ -8,4 +8,8 @@ export default {
     setAuthTokens(authResponseToAuthTokens(res))
     return res
   },
+  signUp: async (email: string, password: string): Promise<IUserCredential> => {
+    const res = (await apiClient.post('/auth/sign-up', { email, password })).data
+    return res
+  },
 }
