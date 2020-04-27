2b2b10589c2e7dadfa83add3a10f418cb688e6b6
Vlad Smirnov <vlad.smirnov@invitae.com>
2020-04-27 11:51:21 +0300

    Add more comments to `useEventual`

--- src/useEventual/index.ts
+++ src/useEventual/index.ts
@@ -10,7 +10,7 @@ import { useAsyncEffect } from '../useAsyncEffect'
  * Typechecker will make sure that you do not accidentally use a value that is not there yet.
  *
  * This hook keeps track of the order in which promises were invoked
- * (this happens each time the dependencies change), and updates the value only when the last
+ * (this happens each time the dependencies change), and updates the value only when the most recet
  * promise resolves. Until then, `{loading: true}` will be returned.
  *
  * @param dataSource Function that returns a promise which resolves to a desired value.
@@ -19,6 +19,13 @@ import { useAsyncEffect } from '../useAsyncEffect'
  */
 export const useEventual = <T>(dataSource: () => Promise<T>, deps: React.DependencyList = []): Eventual<T> => {
   const [state, setState] = React.useState<Eventual<T>>({ loading: true })
+  // `stateVersion` keeps track of how many different promises were passed to this hook.
+  // On each new promise the stateVersion is bumped and is attached to the promise.
+  // When a promise resolves, it's version is compared to the currently expected version,
+  // and promise's value is used only when the versions match. If they don't, that means there's a
+  // more recent promise that should be used.
+  // This helps to achieve a behavior where only the most recent promise is used, regardless of the
+  // order in which they resolve.
   const stateVersion = React.useRef(0)
 
   const incrementVersion = React.useCallback(() => (stateVersion.current = stateVersion.current + 1), [])
